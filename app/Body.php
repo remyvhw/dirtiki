9bc293e7445a8066a0da015d8cc2ce57ab8a777c
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-02-13 15:48:27 -0500

    Updated body model to not use incrementing keys nor timestamps

--- app/Body.php
+++ app/Body.php
@@ -7,6 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class Body extends Model
 {
     /**
+     * Indicates if the IDs are auto-incrementing. They are not.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
      * Get the Page that owns the Body.
      */
     public function page()
