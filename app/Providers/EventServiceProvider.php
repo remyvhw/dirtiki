71521e6a01789835a4a3e1d400c95d5770096e09
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-02-14 12:05:45 -0500

    Removed Page observer as we change strategy

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Observers\PageObserver;
-use App\Page;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
 
@@ -28,7 +26,6 @@ class EventServiceProvider extends ServiceProvider
     public function boot()
     {
         parent::boot();
-        Page::observe(PageObserver::class);
 
     }
 }
