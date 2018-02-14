5d03e4eeb181a682fcc7fb8b5f53a9e75b9f2bf0
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-02-14 11:17:38 -0500

    Completed migration for page redirections

--- database/migrations/2018_02_14_154158_create_page_redirections_table.php
+++ database/migrations/2018_02_14_154158_create_page_redirections_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreatePageRedirectionsTable extends Migration
 {
@@ -16,6 +16,9 @@ class CreatePageRedirectionsTable extends Migration
         Schema::create('page_redirections', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
+            $table->integer('page_id')->unsigned()->index();
+            $table->foreign('page_id')->references('id')->on('pages')->onDelete('cascade');
+            $table->string("slug", 512)->index();
         });
     }
 
