d1b1a2e928323a992f9ca178d8e54688e9c7f309
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-02-14 11:50:05 -0500

    Scaffolded an observer for Page events.

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Event;
+use App\Observers\PageObserver;
+use App\Page;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -26,7 +28,7 @@ class EventServiceProvider extends ServiceProvider
     public function boot()
     {
         parent::boot();
+        Page::observe(PageObserver::class);
 
-        //
     }
 }
