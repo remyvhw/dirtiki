63f2d54a633b28112751f39c0113a87a4a12d825
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-02-15 13:57:41 -0500

    Renamed HistoryResource to AuditResource.

--- app/Http/Resources/AuditResource.php
+++ app/Http/Resources/AuditResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class HistoryResource extends JsonResource
+class AuditResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
