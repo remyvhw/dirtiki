0749246b17dc50686dc4700f8def6e8decffb961
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-02-15 14:37:47 -0500

    Made page factory generate unique names (and slugs).

--- database/factories/PageFactory.php
+++ database/factories/PageFactory.php
@@ -3,7 +3,7 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Page::class, function (Faker $faker) {
-    $name = $faker->sentence(4, true);
+    $name = $faker->unique()->sentence(4, true);
     return [
         "name" => $name,
         "slug" => str_slug($name),
