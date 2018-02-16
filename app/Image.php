1edb5aaf790af7126d63e6d415f0b0277dae574c
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-02-16 12:35:35 -0500

    Made Image model auditable.

--- app/Image.php
+++ app/Image.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class Image extends Model
+class Image extends Model implements Auditable
 {
+    use \OwenIt\Auditing\Auditable;
+
     /**
      * Get the route key for the model.
      *
