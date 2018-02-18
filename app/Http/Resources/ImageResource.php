25df60e6d472d2f62e600d217d827233f1f6d455
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-02-17 23:13:38 -0500

    Updated Image resource to include presets urls.

--- app/Http/Resources/ImageResource.php
+++ app/Http/Resources/ImageResource.php
@@ -14,6 +14,18 @@ class ImageResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        $image = $this;
+        $variations = collect(config("dirtiki.images.presets"))->map(function ($preset) use ($image) {
+            $preset["url"] = route("images.show", array_merge(["image" => $image], $preset), false);
+            return $preset;
+        })->toArray();
+
+        return [
+            "id" => $this->id,
+            "created_at" => $this->created_at,
+            "updated_at" => $this->updated_at,
+            "type" => $this->type,
+            "variations" => $variations,
+        ];
     }
 }
