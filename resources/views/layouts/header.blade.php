f7086be8e6b9d304dbb718d48bed10f420e0b5a4
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-02-18 22:24:59 -0500

    Rewrote the header with Bulma.

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,31 @@
+<section class="section">
+    <div class="container">
+        <nav class="level">
+            <!-- Left side -->
+            <div class="level-left">
+                <div class="level-item">
+                    <p class="title is-5">
+                        <strong>{{ config("app.name") }}</strong>
+                    </p>
+                </div>
+            </div>
+
+            <!-- Right side -->
+            <div class="level-right">
+                @guest
+                <p class="level-item">
+                    <a href="{{ route('register') }}">Register</a>
+                </p>
+                <p class="level-item">
+                    <a href="{{ route('login') }}">Login</a>
+                </p>
+                @else
+                <p class="level-item">
+                    {{ Auth::user()->name }}
+                </p>
+
+                @endguest
+            </div>
+        </nav>
+    </div>
+</section>
