102e2b140ba2033a49a7aa234dd689c3626e2374
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-02-18 23:07:55 -0500

    Moved login page to bulma.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,54 @@
-@extends('layouts.app')
+@extends('layouts.app') @section('content')
+<main class="section">
+    <section class="section">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card card-default">
-                <div class="card-header">Login</div>
+        <div class="columns">
+            <div class="column is-half-desktop is-offset-one-quarter-desktop has-text-centered">
+                <h2 class="title is-2">Login</h2>
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+                    <dirtiki-input 
+                        type="email"
+                        :should-hard-validate-on-blur="true"
+                        label="Email" 
+                        name="email" 
+                        value="{{ old('email') }}"
+                        @if ($errors->has('email'))
+                        message="['{{$errors->first('email')}}']"
+                        @endif
+                        ></dirtiki-input>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">E-Mail Address</label>
+                        <dirtiki-input 
+                        label="Password" 
+                        name="password" 
+                        @if ($errors->has('password'))
+                        message="['{{$errors->first('password')}}']"
+                        @endif
+                        ></dirtiki-input>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                    <div class="field">
+  <div class="control">
+    <label class="checkbox">
+      <input name="remember" type="checkbox" checked>
+      Remember Me
+    </label>
+  </div>
+</div>
 
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+<div class="field is-grouped">
+  <div class="control">
+    <button type="submit" class="button is-primary">Login</button>
+  </div>
+  <div class="control">
+    <a href="{{ route('password.request') }}" class="button is-text">Forgot Your Password?</a>
+  </div>
+</div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
+                </form>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
             </div>
         </div>
-    </div>
-</div>
+    </section>
+</main>
 @endsection
