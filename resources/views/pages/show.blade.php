c97f35bb2f88b95c19d6d05435e12ad00ee06a75
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-02-19 18:22:32 -0500

    Pass an api resource directly to the page-viewer component so there's no need for an ajax call on page load.

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -5,7 +5,9 @@
 
     <section class="container">
         {{ Breadcrumbs::render('pages', $page) }}
-
+        <page-viewer page="{{ (new App\Http\Resources\PageResource($page))->toJson() }}">
+            {{ $page->body->content }}
+        </page-viewer>
     </section>
 </main>
 @endsection
