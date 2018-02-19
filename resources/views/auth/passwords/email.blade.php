2ee41f4f27f3cb6842e182e7e661747a1227aa5c
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-02-19 15:14:49 -0500

    Updated the whole email reset views to Bulma.

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,46 @@
-@extends('layouts.app')
+@extends('layouts.app') @section('content')
+<main class="section">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card card-default">
-                <div class="card-header">Reset Password</div>
+    <section class="container">
+        {{ Breadcrumbs::render('login') }}
+        <div class="columns">
+            <div class="column is-half-desktop is-offset-one-quarter-desktop has-text-centered">
+                <h2 class="title is-2">Reset Password</h2>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                @if (session('status'))
+                <div class="notification is-success">
+                    {{ session('status') }}
                 </div>
+                @endif
+
+                <form method="POST" action="{{ route('password.email') }}">
+                    @csrf
+
+                    <dirtiki-input 
+                        type="email"
+                        :should-hard-validate-on-blur="true"
+                        :required="true"
+                        label="Email" 
+                        name="email" 
+                        value="{{ old('email') }}"
+                        @if ($errors->has('email'))
+                        :error-messages="['{{$errors->first('email')}}']"
+                        @endif
+                        ></dirtiki-input>
+
+                        <div class="field">
+                                <p class="control">
+                                    <button type="submit" class="button is-primary">
+                                            Send Password Reset Link
+                                    </button>
+                                </p>
+                            </div>
+
+
+                </form>
             </div>
         </div>
-    </div>
-</div>
-@endsection
+        </div>
+        </div>
+        </div>
+        @endsection
