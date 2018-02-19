0b9a4fc27bfa323b0adc9489a551c7f327bc10e2
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-02-19 17:22:03 -0500

    HomeController is now only gated with the auth middleware when anonymous views are prohibited.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class HomeController extends Controller
 {
     /**
@@ -13,7 +11,10 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        if (!config("dirtiki.allow_anonymous.views")) {
+            $this->middleware('auth');
+        }
+
     }
 
     /**
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,6 @@
             <div class="card card-default">
                 <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    You are logged in!
-                </div>
             </div>
         </div>
     </div>
