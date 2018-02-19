1e8c0acf81cd0973f412f7d4c070c095bd819f19
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-02-18 19:47:42 -0500

    Hardcoded validation messages inside of script temporarely.

--- public/js/app.js
+++ public/js/app.js
@@ -368,6 +368,88 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
     }
   },
 
+  data: function data() {
+    return {
+      trans: {
+        accepted: "The :attribute must be accepted.",
+        active_url: "The :attribute is not a valid URL.",
+        after: "The :attribute must be a date after :date.",
+        after_or_equal: "The :attribute must be a date after or equal to :date.",
+        alpha: "The :attribute may only contain letters.",
+        alpha_dash: "The :attribute may only contain letters, numbers, and dashes.",
+        alpha_num: "The :attribute may only contain letters and numbers.",
+        array: "The :attribute must be an array.",
+        before: "The :attribute must be a date before :date.",
+        before_or_equal: "The :attribute must be a date before or equal to :date.",
+        between: {
+          numeric: "The :attribute must be between :min and :max.",
+          file: "The :attribute must be between :min and :max kilobytes.",
+          string: "The :attribute must be between :min and :max characters.",
+          array: "The :attribute must have between :min and :max items."
+        },
+        boolean: "The :attribute field must be true or false.",
+        confirmed: "The :attribute confirmation does not match.",
+        date: "The :attribute is not a valid date.",
+        date_format: "The :attribute does not match the format :format.",
+        different: "The :attribute and :other must be different.",
+        digits: "The :attribute must be :digits digits.",
+        digits_between: "The :attribute must be between :min and :max digits.",
+        dimensions: "The :attribute has invalid image dimensions.",
+        distinct: "The :attribute field has a duplicate value.",
+        email: "The :attribute must be a valid email address.",
+        exists: "The selected :attribute is invalid.",
+        file: "The :attribute must be a file.",
+        filled: "The :attribute field must have a value.",
+        image: "The :attribute must be an image.",
+        in: "The selected :attribute is invalid.",
+        in_array: "The :attribute field does not exist in :other.",
+        integer: "The :attribute must be an integer.",
+        ip: "The :attribute must be a valid IP address.",
+        ipv4: "The :attribute must be a valid IPv4 address.",
+        ipv6: "The :attribute must be a valid IPv6 address.",
+        json: "The :attribute must be a valid JSON string.",
+        max: {
+          numeric: "The :attribute may not be greater than :max.",
+          file: "The :attribute may not be greater than :max kilobytes.",
+          string: "The :attribute may not be greater than :max characters.",
+          array: "The :attribute may not have more than :max items."
+        },
+        mimes: "The :attribute must be a file of type: :values.",
+        mimetypes: "The :attribute must be a file of type: :values.",
+        min: {
+          numeric: "The :attribute must be at least :min.",
+          file: "The :attribute must be at least :min kilobytes.",
+          string: "The :attribute must be at least :min characters.",
+          array: "The :attribute must have at least :min items."
+        },
+        not_in: "The selected :attribute is invalid.",
+        numeric: "The :attribute must be a number.",
+        present: "The :attribute field must be present.",
+        regex: "The :attribute format is invalid.",
+        required: "The :attribute field is required.",
+        required_if: "The :attribute field is required when :other is :value.",
+        required_unless: "The :attribute field is required unless :other is in :values.",
+        required_with: "The :attribute field is required when :values is present.",
+        required_with_all: "The :attribute field is required when :values is present.",
+        required_without: "The :attribute field is required when :values is not present.",
+        required_without_all: "The :attribute field is required when none of :values are present.",
+        same: "The :attribute and :other must match.",
+        size: {
+          numeric: "The :attribute must be :size.",
+          file: "The :attribute must be :size kilobytes.",
+          string: "The :attribute must be :size characters.",
+          array: "The :attribute must contain :size items."
+        },
+        string: "The :attribute must be a string.",
+        timezone: "The :attribute must be a valid zone.",
+        unique: "The :attribute has already been taken.",
+        uploaded: "The :attribute failed to upload.",
+        url: "The :attribute format is invalid."
+      }
+    };
+  },
+
+
   computed: {
     htmlType: function htmlType() {
       switch (this.type) {
@@ -490,50 +572,50 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
       // Check for required fields.
       if (this.required && this.value === "") {
         valid = false;
-        messages.push(_.replace(this.trans.required, ":attribute", this.label));
+        messages.push(this.trans.required.replace(":attribute", this.label));
       }
 
       // Check length
       if (this.isMinLengthInvalid()) {
         valid = false;
-        messages.push(_.replace(_.replace(this.trans.min.string, ":min", this.minLength), ":attribute", this.label));
+        var message = messages.push(this.trans.min.string.replace(":min", this.minLength).replace(":attribute", this.label));
       }
       if (this.isMaxLengthInvalid()) {
         valid = false;
-        messages.push(_.replace(_.replace(this.trans.max.string, ":max", this.maxLength), ":attribute", this.label));
+        messages.push(this.trans.max.string.replace(":max", this.maxLength).replace(":attribute", this.label));
       }
 
       // Check for min and max values for numeric fields
       if ((this.type == "number" || this.type == "digit") && this.min != null && this.value < this.min) {
         valid = false;
-        messages.push(_.replace(_.replace(this.trans.min.numeric, ":min", this.min), ":attribute", this.label));
+        messages.push(this.trans.min.numeric.replace(":min", this.min).replace(":attribute", this.label));
       } else if ((this.type == "number" || this.type == "digit") && this.max != null && this.value > this.max) {
         valid = false;
-        messages.push(_.replace(_.replace(this.trans.max.numeric, ":max", this.max), ":attribute", this.label));
+        messages.push(this.trans.max.numeric.replace(":max", this.max).replace(":attribute", this.label));
       }
 
       // Email validation
       if (this.type == "email" && this.isEmailInvalid()) {
         valid = false;
-        messages.push(_.replace(this.trans.email, ":attribute", this.label));
+        messages.push(this.trans.email.replace(":attribute", this.label));
       }
 
       // Number validation (for decimal steps)
       if (this.type == "number" && this.step % 1 != 0 && this.isNumberInvalid()) {
         valid = false;
-        messages.push(_.replace(this.trans.numeric, ":attribute", this.label));
+        messages.push(this.trans.numeric.replace(":attribute", this.label));
       }
 
       // Digit validation (or number with an integer step)
       if ((this.type == "digit" || this.type == "number" && this.step % 1 == 0) && this.isDigitsInvalid()) {
         valid = false;
-        messages.push(_.replace(this.trans.integer, ":attribute", this.label));
+        messages.push(this.trans.integer.replace(":attribute", this.label));
       }
 
       // Exact match validation
       if (this.mustMatch && this.mustMatch != this.value) {
         valid = false;
-        messages.push(_.replace(this.trans.confirmed, ":attribute", this.label));
+        messages.push(this.trans.confirmed.replace(":attribute", this.label));
       }
 
       this.error = !valid;
