51689cc26139be9e191df3593887ed2b607663ce
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-02-19 18:04:19 -0500

    Updated menu to Bulma navbar.

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,33 +1,29 @@
-<section class="section">
-    <div class="container">
-        <nav class="level">
-            <!-- Left side -->
-            <div class="level-left">
-                <div class="level-item">
-                    <p class="title is-5">
-                        <strong>
-                            <a href="{{ route('home') }}">{{ config("app.name") }}</a>
-                        </strong>
-                    </p>
-                </div>
-            </div>
-
-            <!-- Right side -->
-            <div class="level-right">
-                @guest
-                <p class="level-item">
-                    <a href="{{ route('register') }}">Register</a>
-                </p>
-                <p class="level-item">
-                    <a href="{{ route('login') }}">Login</a>
-                </p>
-                @else
-                <p class="level-item">
-                    {{ Auth::user()->name }}
-                </p>
-
-                @endguest
-            </div>
-        </nav>
+<nav class="navbar is-light" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+        <div class="navbar-item">
+            <strong>
+                <a href="{{ route('home') }}">{{ config("app.name") }}</a>
+            </strong>
+        </div>
     </div>
-</section>
+
+    <div class="navbar-end">
+
+        @guest
+        <a class="navbar-item" href="{{ route('register') }}">
+            Register
+        </a>
+        <a class="navbar-item" href="{{ route('login') }}">
+            Login
+        </a>
+
+        @else
+        <a class="navbar-item" href="#">
+            {{ Auth::user()->name }}
+        </a>
+
+
+        @endguest
+
+    </div>
+</nav>
