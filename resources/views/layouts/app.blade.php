83e470f549f142b8dde4fb06f980b8bb18e66a04
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-02-18 21:51:03 -0500

    Updated app name in blade.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,17 +3,15 @@
 
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>@yield('title', config("app.name"))</title>
 
         <!-- Styles -->
         <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
-
         <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     </head>
 
