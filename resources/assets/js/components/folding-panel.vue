7266c2d854e916c9bf61d579858358956f36f078
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-02-21 10:46:42 -0500

    Scaffolded a folding panel component.

---
+++ resources/assets/js/components/folding-panel.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="panel">
+        <p class="panel-heading">
+            {{ title }}
+        </p>
+        <div class="panel-block">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script type="text/babel">
+export default {
+  props: {
+    deployed: { Type: Boolean, Default: true },
+    title: { Type: String, Required: true }
+  }
+};
+</script>
