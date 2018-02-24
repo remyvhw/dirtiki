51635abef2671565925d4e71f142039cba8e5303
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-02-24 12:47:07 -0500

    Temporarely use pages index view as home controller index.

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,10 +1,7 @@
 @extends('layouts.app') @section('content')
 <main class="section">
     <section class="container">
-        {{ Breadcrumbs::render('page', $page) }} @if((new App\Policies\PagePolicy)->update(Auth::user(), $page))
-        <a href="{{ route(" pages.edit ", [$page]) }}" class="button is-pulled-right">Edit</a>
-        @endif
-        <page-viewer page-json="{{ (new App\Http\Resources\PageResource($page))->toJson() }}">{{ $page->body->content }}</page-viewer>
+        {{ Breadcrumbs::render('home') }}
     </section>
 </main>
 @endsection
