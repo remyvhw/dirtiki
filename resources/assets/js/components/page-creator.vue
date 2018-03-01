48abe87a74a50798b94dbd5e1c8b71a717795e89
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-03-01 17:18:21 -0500

    Scaffolded a page creator component.

---
+++ resources/assets/js/components/page-creator.vue
@@ -0,0 +1,26 @@
+<template>
+    <article>
+
+        <metadata-editor @input="reloadWithUpdatedMetadata" :page="page"></metadata-editor>
+
+        <body-editor @input="reloadWithUpdatedBody" :body="page.relationships.body"></body-editor>
+
+    </article>
+</template>
+
+<script type="text/babel">
+export default {
+  components: {
+    metadataEditor: require("./metadata-editor.vue"),
+    bodyEditor: require("./body-editor.vue")
+  },
+  props: {},
+  data() {
+    return {
+      page: null
+    };
+  },
+  mounted() {},
+  methods: {}
+};
+</script>
