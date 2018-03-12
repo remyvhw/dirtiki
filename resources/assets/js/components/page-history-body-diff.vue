6ba997549601690a3f81d58d6ec786231695e6d9
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-03-12 19:28:51 -0400

    Removed jsdiff for performance reasons.

--- resources/assets/js/components/page-history-body-diff.vue
+++ resources/assets/js/components/page-history-body-diff.vue
@@ -1,14 +1,11 @@
 <template>
     <div class="diff">
         <history-metadata :diff="diff"></history-metadata>
-        <button @click="computeDiff">Generate</button>
-        <pre v-if="computedDiff" v-html="computedDiff"></pre>
+        <pre></pre>
     </div>
 
 </template>
 <script type="text/babel">
-var jsDiff = require("diff");
-
 export default {
   components: {
     historyMetadata: require("./history-metadata.vue")
@@ -21,34 +18,10 @@ export default {
   },
   computed: {},
   data() {
-    return {
-      computedDiff: null
-    };
+    return {};
   },
   methods: {
-    computeDiff() {
-      let diff = jsDiff.diffWordsWithSpace(
-        this.diff.data.changes.before.content,
-        this.diff.data.changes.after.content
-      );
-
-      let fragment = document.createDocumentFragment(),
-        color = "",
-        span = null;
-
-      diff.forEach(function(part) {
-        color = part.added ? "green" : part.removed ? "red" : "grey";
-        span = document.createElement("span");
-        span.style.color = color;
-        span.appendChild(document.createTextNode(part.value));
-        fragment.appendChild(span);
-      });
-
-      let container = document.createElement("pre");
-      container.appendChild(fragment);
-      debugger;
-      this.computedDiff = container.innerHTML;
-    }
+    computeDiff() {}
   }
 };
 </script>
