fb902055e79ab3e23d33d8e79e51bdca0f88b372
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-03-16 11:37:02 -0400

    Added a history button to pages.

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.app') @section('content')
 <div class="section">
     <section class="container">
-        {{ Breadcrumbs::render('page', $page) }} @if((new App\Policies\PagePolicy)->update(Auth::user(), $page))
-        <a href="{{ route('pages.edit', [$page]) }}" class="button is-pulled-right">Edit</a>
+        {{ Breadcrumbs::render('page', $page) }} 
+        <div class="has-text-right">
+        <a href="{{ route('pages.history', [$page]) }}" class="button">History</a>
+        @if((new App\Policies\PagePolicy)->update(Auth::user(), $page))
+        <a href="{{ route('pages.edit', [$page]) }}" class="button">Edit</a>
         @endif
+        </div>
     </section>
 </div>
 <page-viewer page-json="{{ (new App\Http\Resources\PageResource($page))->toJson() }}">{{ $page->body->content }}</page-viewer>
