39ea4c49042addce4521b74184a0687fb22d0e41
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-03-15 23:54:23 -0400

    Added a (dummy) frontend localization file.

---
+++ resources/lang/en/frontend.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Frontend Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by frontend assets. They are loaded
+    | by via a GET request on `/api/strings/{language}`.
+    |
+     */
+
+    'failed' => 'These credentials do not match our records.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+];
