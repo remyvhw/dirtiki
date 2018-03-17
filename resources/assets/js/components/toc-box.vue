e38266f3c1856d587bdff51fcee09ae400b60695
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-03-17 00:50:29 -0400

    Scaffolded a toc box component.

---
+++ resources/assets/js/components/toc-box.vue
@@ -0,0 +1,19 @@
+<style scoped>
+
+</style>
+<template>
+    <div class="box">
+        <h1>Hello</h1>
+    </div>
+</template>
+
+<script type="text/babel">
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  mounted() {}
+};
+</script>
