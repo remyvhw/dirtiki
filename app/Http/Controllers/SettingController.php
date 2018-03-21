87eb9b08f1ad1df86fbd4d7b4d7760f5f407d6dc
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-03-21 16:10:01 -0400

    Added a basic structure for settings metadata.

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -3,14 +3,84 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Setting;
+
+class DirtikiSetting
+{
+    public $structure;
+    public $key;
+    public function __construct(array $structure, string $key)
+    {
+        $this->structure = $structure;
+        $this->key = $key;
+    }
+
+    public function children()
+    {
+        $keyPrefix = $this->key;
+        return collect(array_get($this->structure, "children", []))->mapWithKeys(function ($structure, $key) use ($keyPrefix) {
+            return new DirtikiSetting($structure, "{$keyPrefix}.{$key}");
+        });
+    }
+
+    public function label()
+    {
+        return array_get($this->structure, "label");
+    }
+
+    function default() {
+        return array_get($this->structure, "default", null);
+    }
+
+    public function getValue()
+    {
+        return Setting::get($this->key, $this->default());
+    }
+
+    public function setValue($value)
+    {
+        return Setting::set($this->key, $value);
+    }
+
+    public function __toString()
+    {
+        return collect($this)->toJson();
+    }
+
+}
 
 class SettingController extends Controller
 {
+
+    public function getSettingsMetadata()
+    {
+        return collect([
+            "general" => [
+                "label" => __("General"),
+                "children" => [
+                    "app_name" => [
+                        "label" => __("Application Name"),
+                        "validation" => "required|min:1|max:32",
+                    ],
+                ],
+            ],
+        ])->mapWithKeys(function ($structure, $key) {
+            return [$key => new DirtikiSetting($structure, $key)];
+        });
+    }
 
     public function getEdit(Request $request, string $group)
     {
         $this->authorize("update-settings");
 
+        $dirtikiSettings = $this->getSettingsMetadata();
+        if (!($dirtikiSetting = data_get($dirtikiSettings, $group))) {
+            abort(404);
+        }
+
+        $settings = $dirtikiSetting;
+
+        return $settings;
     }
 
     public function postUpdate(Request $request, string $group)
