1226de8f2b1eb4f62f5244f2b2870d9634f089a3
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-03-21 14:45:33 -0400

    Gated setting controller.

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -6,13 +6,15 @@ use Illuminate\Http\Request;
 
 class SettingController extends Controller
 {
+
     public function getEdit(Request $request, string $group)
     {
+        $this->authorize("update-settings");
 
     }
 
     public function postUpdate(Request $request, string $group)
     {
-
+        $this->authorize("update-settings");
     }
 }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -26,6 +27,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('update-settings', function ($user) {
+            return (bool) $user->admin;
+        });
     }
 }
