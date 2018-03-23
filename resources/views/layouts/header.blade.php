f9e5b2c18f7588b0a5f93589dbc3dd929f76f948
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-03-23 16:00:35 -0400

    Fixed header padding to it's consistent with the rest of the page.

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,41 +1,41 @@
 <nav class="navbar is-light" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-        <div class="navbar-item">
-            <strong>
-                <a href="{{ route('home') }}">{{ config("app.name") }}</a>
-            </strong>
-        </div>
-    </div>
-
-    <div class="navbar-end">
-
-        @if(config("scout.driver"))
-        <div class="navbar-item">
-            <div class="control has-icons-right">
-                <input class="input" v-model="query" placeholder="Search">
-
-                <span class="icon is-small is-right">
-                    <i class="fas fa-search"></i>
-                </span>
+    <div class="container">
+        <div class="navbar-brand">
+            <div class="navbar-item">
+                <strong>
+                    <a href="{{ route('home') }}">{{ config("app.name") }}</a>
+                </strong>
             </div>
         </div>
-        @endif
-        
-        @guest
-        <a class="navbar-item" href="{{ route('register') }}">
-            Register
-        </a>
-        <a class="navbar-item" href="{{ route('login') }}">
-            Login
-        </a>
 
-        @else
-        <a class="navbar-item" href="#">
-            {{ Auth::user()->name }}
-        </a>
+        <div class="navbar-end">
+
+            @if(config("scout.driver"))
+            <div class="navbar-item">
+                <div class="control has-icons-right">
+                    <input class="input" v-model="query" placeholder="Search">
+
+                    <span class="icon is-small is-right">
+                        <i class="fas fa-search"></i>
+                    </span>
+                </div>
+            </div>
+            @endif @guest
+            <a class="navbar-item" href="{{ route('register') }}">
+                Register
+            </a>
+            <a class="navbar-item" href="{{ route('login') }}">
+                Login
+            </a>
+
+            @else
+            <a class="navbar-item" href="#">
+                {{ Auth::user()->name }}
+            </a>
 
 
-        @endguest
+            @endguest
 
+        </div>
     </div>
 </nav>
