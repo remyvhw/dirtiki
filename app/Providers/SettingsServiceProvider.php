e707f4625b8d10608f0fd2cb7d02577a050245d5
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-04-09 13:00:12 -0400

    Fixed an issue that prevented migrations from running from a blank state.

--- app/Providers/SettingsServiceProvider.php
+++ app/Providers/SettingsServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Schema;
 use Setting;
 
 class SettingsServiceProvider extends ServiceProvider
@@ -14,6 +15,10 @@ class SettingsServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Avoid relying on settings if no migration has been ran yet.
+        if (Schema::hasTable('settings')) {
+            return;
+        }
 
         config(["app.name" => Setting::get("general.app_name", "This is Dirtiki")]);
 
