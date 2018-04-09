c800f0db93963107265e37fe8b5db30f2548e8a0
Remy Vanherweghem <remy.vanherweghem@pumax.info>
2018-04-08 20:32:59 -0400

    Required google recptcha package.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -69,7 +69,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-
+        dd($data);
         $user = new User([
             'name' => $data['name'],
             'email' => $data['email'],
